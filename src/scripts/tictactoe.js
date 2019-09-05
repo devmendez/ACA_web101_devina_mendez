d7d30b6690d18b7c79cce2485b65243b25d65644
devmendez <devmendez@gmail.com>
2019-09-04 23:30:03 -0500

    tictactoe

---
+++ src/scripts/tictactoe.js
@@ -0,0 +1,21 @@
+var gameMarker = X;
+var gameMarker = O;
+
+function changeMarkerToX(gameMarker) {
+  console.log("The x button was clicked!");
+}
+function changeMarkerToO(gameMarker) {
+  console.log("The o button was clicked!");
+}
+
+function placeMark(gameMarker) {
+  document.getElementById(TL);
+  document.getElementById(TM);
+  document.getElementById(TR);
+  document.getElementById(ML);
+  document.getElementById(MM);
+  document.getElementById(MR);
+  document.getElementById(BL);
+  document.getElementById(BM);
+  document.getElementById(BR);
+}
