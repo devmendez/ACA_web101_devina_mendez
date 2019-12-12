74fee9823d1f5a2015d351b2a8e98496af384b9e
devmendez <devmendez@gmail.com>
2019-12-11 21:41:23 -0600

    node_mysql

---
+++ JS 311/311_wk4_day2_node_mysql/controllers/users.js
@@ -0,0 +1,67 @@
+const mysql = require('mysql')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
+const getAllUsers = (req, res) => {
+  // SELECT ALL USERS
+  pool.query("SELECT * FROM users", (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+}
+
+const getUserById = (req, res) => {
+  // SELECT USERS WHERE ID = <REQ PARAMS ID>
+  let sql = 'SELECT * FROM ?? WHERE ?? = ?';
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, ['users', 'id', req.params.id]);
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+}
+
+const createUser = (req, res) => {
+  // INSERT INTO USERS FIRST AND LAST NAME 
+  let sql = 'INSERT INTO ?? (??, ??) VALUES (?, ?)'
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, ['users', 'first_name', 'last_name', req.body.first_name, req.body.last_name])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ newId: results.insertId });
+  })
+}
+
+const updateUserById = (req, res) => {
+  // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
+  let sql = 'UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?';
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, ['users', 'first_name', req.body.first_name, 'last_name', req.body.last_name, 'id', req.params.id])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.status(204).json();
+  })
+}
+
+const deleteUserByFirstName = (req, res) => {
+  // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
+  let sql = 'DELETE FROM ?? WHERE ?? = ?';
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, ['users', 'first_name', req.params.first_name])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
+  })
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUserByFirstName
+}
